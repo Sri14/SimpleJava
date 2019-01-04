563df0bebe6e4e05abe44fae0e9bd2e4c10be20e
Sri Padma <sri@Sris-MacBook-Pro.local>
2019-01-04 23:25:26 +0000

    second commit

---
+++ src/addition/AddSum1.java
@@ -0,0 +1,15 @@
+package addition;
+
+public class AddSum1 {
+
+	public static void main(String[] args) {
+		
+		int a =30;
+		int b =50;
+		int c =a+b;
+		System.out.println("Value of c = " + c);
+	
+
+	}
+
+}
