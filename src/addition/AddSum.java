7e3e360691d9b390a433e3be55c2242914cf2b43
Sri Padma <sri@Sris-MacBook-Pro.local>
2019-01-05 21:22:15 +0000

    modification

--- src/addition/AddSum.java
+++ src/addition/AddSum.java
@@ -12,5 +12,19 @@ public class AddSum {
 		int c=a+b;
 		System.out.println("The value of c = "+c);
 	}
+	
+	@Test
+	public void sumWithForLoop(){
+		
+		int a =30;
+		int b =50;
+		int c =a+b;
+		System.out.println("Value of c = " + c);
+	
+		
+		for(int i=0; i<10; i++ ){
+			System.out.println("Helllo world "+ i);
+	}
 
+	}
 }
